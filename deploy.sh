06736b696bab3c61bcf04c5468c6c6241b68b824
uzimihsr <uzimihsr@gmail.com>
2025-10-11 22:45:47 +0900

    デプロイスクリプトを修正

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-set -ev
+set -e
 
-# rgName='dify-001' # CHANGE ME!!!
-
-echo "AZ CLIでログイン"
-az login
-
-echo "リソースグループを作成"
+echo "リソースグループ $rgName を作成"
 az group create --name $rgName --location japaneast
 
 echo "リソースグループに必要なリソースを作成"
@@ -15,15 +10,16 @@ az deployment group create \
   -f main.bicep \
   -p main.bicepparam
 
-
-echo "マウントが必要なファイルをAzure File共有にアップロード"
+echo "一部のコンテナでマウントが必要なファイルをAzure File共有にアップロード"
+sleep 10
 stName=$(az storage account list -g $rgName --query "[0].name" -o tsv)
 stKey=$(az storage account keys list -g $rgName -n $stName --query "[0].value" -o tsv)
 az storage file upload-batch --account-name $stName --account-key $stKey --destination "volume-nginx" --source ./volumes/nginx
 az storage file upload-batch --account-name $stName --account-key $stKey --destination "volume-dify-sandbox" --source ./volumes/sandbox
 az storage file upload-batch --account-name $stName --account-key $stKey --destination "volume-ssrf-proxy" --source ./volumes/ssrf_proxy 
 
-echo "Container Appsを再起動"
+echo "マウント後のコンテナを再起動"
+sleep 10
 ssrfProxyRevision=$(az containerapp revision list -g $rgName -n "ssrf-proxy" --query "[0].name" -o tsv)
 az containerapp revision restart -g $rgName -n "ssrf-proxy" --revision $ssrfProxyRevision
 sandboxRevision=$(az containerapp revision list -g $rgName -n "sandbox" --query "[0].name" -o tsv)
@@ -31,7 +27,9 @@ az containerapp revision restart -g $rgName -n "sandbox" --revision $sandboxRevi
 nginxRevision=$(az containerapp revision list -g $rgName -n "nginx" --query "[0].name" -o tsv)
 az containerapp revision restart -g $rgName -n "nginx" --revision $nginxRevision
 
-echo "ブラウザで https://$(az containerapp show -g $rgName -n nginx --query "properties.configuration.ingress.fqdn" -o tsv) を開きます..."
+fqdn=$(az containerapp show -g $rgName -n "nginx" --query "properties.configuration.ingress.fqdn" -o tsv)
+echo "ブラウザで https://${fqdn} を開きます..."
+sleep 5
 az containerapp browse -g $rgName -n "nginx"
 
 echo "Enjoy!"
